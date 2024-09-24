40c7a085c5c6adb51b6fe63b888089a12e719fae
Inal Mahpud <inalmahpud1@gmail.com>
2024-09-24 22:04:14 +0700

    add tugas praktikum bootstrap

---
+++ 06_BOOTSTRAP/praktikum/assets/index.js
@@ -0,0 +1,32 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+  const alert = document.getElementById('alert');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          alert.classList.remove("d-none"); 
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
